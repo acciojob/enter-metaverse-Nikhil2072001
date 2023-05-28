8996dbf9396d69103f2b603538637d4a011b1970
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-05-28 12:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function press(){
+	console.log("hy");
+ 	var p = document.getElementById('status');
+	p.innerText="";
+	var h = document.createElement('h1');
+	h.innerText  = "Entered Metaverse";
+document.body.appendChild(h);
+	
+}
